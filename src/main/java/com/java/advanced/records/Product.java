b0118ad3b3293da4d008348fdd5297d428759366
thilak0805 <daya.22.thilak@gmail.com>
2025-07-06 17:48:49 +0530

    test case for cost,type,custom constructor

--- src/main/java/com/java/advanced/records/Product.java
+++ src/main/java/com/java/advanced/records/Product.java
@@ -10,4 +10,21 @@ import java.math.BigDecimal;
 public record Product(String name,
                       BigDecimal cost,
                       String type) {
+    // by default constructors are created by compiler those are called canonical constructors
+
+    // below is not a constructor a simple code block that has access to all other parameters present above
+    public Product{
+        if(name!=null && name.isBlank()){
+            throw new IllegalArgumentException("name is not valid");
+        }
+        if(cost.compareTo(BigDecimal.ZERO)<=0){
+            throw new IllegalArgumentException("cost value is not valid");
+        }
+    }
+
+    //create custom constructor
+    //the custom constructor should always call the
+    public Product(String name, BigDecimal cost){
+        this(name,cost, "GENERAL");
+    }
 }
--- src/test/java/com/java/advanced/records/ProductTest.java
+++ src/test/java/com/java/advanced/records/ProductTest.java
@@ -22,4 +22,47 @@ class ProductTest {
 
     }
 
+    @Test
+    void createProduct_nameNotValid(){
+        //given
+        var product = new Product("IPhone",
+                new BigDecimal("99.99"),
+                "Electronics");
+        //when
+        var exception = assertThrows(IllegalArgumentException.class,
+                ()->new Product("",
+                new BigDecimal("99.99"),
+                "Electronics"));
+        //then
+        assertEquals("name is not valid", exception.getMessage());
+
+    }
+
+    @Test
+    void createProduct_costNotValid(){
+        //given
+
+        //when
+        var exception = assertThrows(IllegalArgumentException.class,
+                ()->new Product("IPhone",
+                        new BigDecimal("-99.99"),
+                        "Electronics"));
+        //then
+        assertEquals("cost value is not valid", exception.getMessage());
+    }
+
+    @Test
+    void createProduct_customConstructor(){
+        //given
+        //not passing type below, in order to call the custom constructor
+        var product = new Product("IPhone",
+                new BigDecimal("99.99")
+                );
+        //when
+        System.out.println("product---"+product);
+        assertEquals("IPhone", product.name());
+        assertEquals("GENERAL", product.type());
+
+    }
+
 }
